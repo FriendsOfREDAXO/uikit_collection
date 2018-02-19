bba360581db137806c0a0eee181969da61c1db6e
Thomas Skerbis <thomas.skerbis@mac.com>
2018-02-19 22:55:45 +0100

    unnötige sachen entfernt

--- plugins/login_image/install.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$plugin = rex_plugin::get('klxm_defaults', 'login_image');
-
-if (!$plugin->hasConfig()) {
-    $plugin->setConfig('bild', 'out5_login_image.jpg');
-    $plugin->setConfig('status', 'deaktiviert');    
-}
