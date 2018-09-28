95a8460a98e8d57c2af0c3f93c7bc051dc365473
Thomas Skerbis <thomas.skerbis@mac.com>
2018-09-28 22:03:53 +0200

    beta6
    - aktueller release candidate
    - gitignore

--- boot.php
+++ boot.php
@@ -21,7 +21,7 @@ if (rex::isBackend() && rex::getUser()) {
             if (rex_request_method() == 'get' && $this->getConfig('check_scss') == '1') {
 
                 $compiler = new rex_scss_compiler();
-                $scss_files = rex_extension::registerPoint(new rex_extension_point('BE_STYLE_SCSS_FILES', [$this->getPath('SCSS/uikit/own.scss')]));
+                $scss_files = rex_extension::registerPoint(new rex_extension_point('BE_STYLE_SCSS_FILES', [$this->getPath('SCSS/uikit/my-theme.scss')]));
                 $compiler->setRootDir(rex_path::addon('uikit_collection'));
                 $compiler->setScssFile($scss_files);
                 $compiler->setCssFile($this->getPath('assets/uikit/css/uikit.css'));
