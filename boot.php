9d7997899f463533e8ec8d8337ed6986f0d5c2f5
Thomas Skerbis <skerbis@klxm.de>
2019-04-16 15:21:30 +0200

    styles

--- boot.php
+++ boot.php
@@ -1,6 +1,7 @@
 <?php
 if (rex::isBackend() && rex::getUser()) {
     rex_perm::register('uikit_modul[]');
+    rex_view::addCssFile($this->getAssetsUrl('css/styles.css'));
         rex_view::addCssFile($this->getAssetsUrl('uikit_backend/css/uikit.css'));
         rex_view::addJsFile($this->getAssetsUrl('uikit_backend/js/uikit.min.js'));
         rex_view::addJsFile($this->getAssetsUrl('uikit_backend/js/uikit-icons.min.js'));
