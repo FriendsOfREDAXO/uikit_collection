1aba9d4a1068811856b742dd3bec0eb87e1f697c
Thomas Skerbis <skerbis@klxm.de>
2019-04-26 20:52:34 +0200

    $id entfernt

--- inc/functions.php
+++ inc/functions.php
@@ -1,15 +1,12 @@
 <?php
-
+// Function zur Ermittlung eines Linktyps 
 if (!function_exists('infolink')) {
-
-    function infolink($url) {
-
+    function infolink($url)
+    {
         // Wurde ein Wert für $url übergeben?
         if ($url) {
-
             // Prüfe ob es sich um eine URL handelt, dann weiter
             if (filter_var($url, FILTER_VALIDATE_URL) === FALSE) {
-                
             }
             // Ist es eine Mediendatei?
             if (file_exists(rex_path::media($url)) === true) {
@@ -24,88 +21,79 @@ if (!function_exists('infolink')) {
             return $link;
         }
     }
-
 }
-
 // Function zur Generierung eines Navigationsarrays
 // Benötigt für den Aufruf werden nur $start,$depth und $ignoreoffline
 // Alle weiteren Angaben dienen der internen Verarbeitung
 // Alle weiteren Informationen aus rex_structure fintet man in catObject
-
 if (!function_exists('structureArray')) {
-function structureArray($start = 0, $depth = 0, $ignoreOffline = true, $depth_saved = 0, $level = 0, $id = 0)
-{
-    $result = array();
-    // Aktuelle Kategorie ermitteln
-    $currentCat     = rex_category::getCurrent();
-            if ($currentCat)
-            {
+    function structureArray($start = 0, $depth = 0, $ignoreOffline = true, $depth_saved = 0, $level = 0)
+    {
+        $result     = array();
+        // Aktuelle Kategorie ermitteln
+        $currentCat = rex_category::getCurrent();
+        if ($currentCat) {
             $currentCatpath = $currentCat->getPathAsArray();
             $currentCat_id  = $currentCat->getId();
-            }
-            else
-            {
+        } else {
             $currentCatpath = array();
             $currentCat_id  = 0;
+        }
+        if ($start != 0) {
+            $startCat  = rex_category::get($start);
+            $startPath = $startCat->getPathAsArray();
+            $depth     = count($startPath) + $depth;
+            $startCats = $startCat->getChildren($ignoreoffline);
+            if ($depth_saved != 0) {
+                $depth = $depth_saved;
+            } else {
+                $depth_saved = $depth;
             }
-    
-    
-    if ($start != 0) {
-        $startCat  = rex_category::get($start);
-        $startPath = $startCat->getPathAsArray();
-        $depth     = count($startPath) + $depth;
-        $startCats = $startCat->getChildren($ignoreoffline);
-        if ($depth_saved != 0) {
-            $depth = $depth_saved;
         } else {
-            $depth_saved = $depth;
+            $startCats = rex_category::getRootCategories(true);
+            $depth     = $depth;
         }
-    } else {
-        $startCats = rex_category::getRootCategories(true);
-        $depth     = $depth;
-    }
-    if ($startCats) {
-        foreach ($startCats as $cat) {
-            $children['child'] = array();
-            $hasChildren       = false;
-            $catId             = $cat->getId();
-            $path              = $cat->getPathAsArray();
-            $listlevel         = count($path);
-            if ($listlevel > $depth) {
-                continue;
+        if ($startCats) {
+            foreach ($startCats as $cat) {
+                $children['child'] = array();
+                $hasChildren       = false;
+                $catId             = $cat->getId();
+                $path              = $cat->getPathAsArray();
+                $listlevel         = count($path);
+                if ($listlevel > $depth) {
+                    continue;
+                }
+                if ($listlevel <= $depth && $depth != 0 && $cat && $cat->getChildren($ignoreoffline)) {
+                    $level++;
+                    $hasChildren       = true;
+                    // Unterkategorien ermitteln, function ruft sich selbst auf
+                    $children['child'] = structureArray($catId, $depth, $ignoreOffline = true, $depth_saved, $level);
+                    $level--;
+                }
+                // Name der Kategorie
+                $catName = $cat->getName();
+                // Url ermitteln
+                $catUrl  = $cat->getUrl();
+                // Aktiven Pfad ermitteln
+                $active  = false;
+                if (in_array($catId, $currentCatpath) or $currentCat_id == $catId) {
+                    $active = true;
+                }
+                // Ergebnis speichern
+                $result[] = array(
+                    'catId' => $catId,
+                    'parentId' => $start,
+                    'level' => $level,
+                    'active' => $active,
+                    'catName' => $catName,
+                    'url' => $catUrl,
+                    'hasChildren' => $hasChildren,
+                    'children' => $children['child'],
+                    'path' => $path,
+                    'catObject' => $cat
+                );
             }
-            if ($listlevel <= $depth && $depth != 0 && $cat && $cat->getChildren($ignoreoffline)) {
-                $level++;
-                $hasChildren       = true;
-                // Unterkategorien ermitteln, function ruft sich selbst auf
-                $children['child'] = structureArray($catId, $depth, $ignoreOffline = true, $depth_saved, $level);
-                $level--;
-            }
-            // Name der Kategorie
-            $catName        = $cat->getName();
-            // Url ermitteln
-            $catUrl         =  $cat->getUrl();
-            // Aktiven Pfad ermitteln
-            $active         = false;          
-            if (in_array($catId, $currentCatpath) or $currentCat_id == $catId) {
-                $active = true;
-            }
-            // Ergebnis speichern
-            $result[] = array(
-                'catId' => $catId,
-                'parentId' => $start,
-                'level' => $level,
-                'active' => $active,
-                'catName' => $catName,
-                'url' => $catUrl,
-                'hasChildren' => $hasChildren,
-                'children' => $children['child'],
-                'path' => $path,
-                'catObject' => $cat
-            );
-        
         }
+        return $result;
     }
-    return $result;
-}
 }
