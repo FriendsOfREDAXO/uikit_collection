458956b727f50a7170b61a16d7f2db498d127e20
Thomas Skerbis <thomas.skerbis@mac.com>
2022-01-26 17:30:44 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -5,12 +5,13 @@ if (rex_plugin::get('yform', 'manager')->isAvailable()) {
 
 if (rex::isBackend() && rex::getUser()) {
     rex_perm::register('uikit_modul[]');
-    rex_view::addCssFile($this->getAssetsUrl('css/styles.css'));
+    // Prevent loading assets for backend when UIKit3 demo is available
+    if (!rex_addon::get('demo_uikit3')->isAvailable()) {
         rex_view::addCssFile($this->getAssetsUrl('uikit/css/uikit.min.css'));
         rex_view::addJsFile($this->getAssetsUrl('uikit/js/uikit.min.js'));
         rex_view::addJsFile($this->getAssetsUrl('uikit/js/uikit-icons.min.js'));
+    }
 }
-
     rex_extension::register('PACKAGES_INCLUDED', function($ep) {
         include_once rex_path::addon('uikit_collection/inc').'infolink_func.php';
     });
