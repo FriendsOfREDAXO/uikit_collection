9e02e0a16b831b33f89369b0949680bc9462a421
Thomas Skerbis <skerbis@klxm.de>
2022-01-27 15:23:23 +0100

    assets verschoben

--- boot.php
+++ boot.php
@@ -5,14 +5,11 @@ if (rex_plugin::get('yform', 'manager')->isAvailable()) {
 
 if (rex::isBackend() && rex::getUser()) {
     rex_perm::register('uikit_modul[]');
-    // Prevent loading assets for backend when UIKit3 demo is available
-    if (!rex_addon::get('demo_uikit3')->isAvailable()) {
-        rex_view::addCssFile($this->getAssetsUrl('uikit/css/uikit.min.css'));
-        rex_view::addJsFile($this->getAssetsUrl('uikit/js/uikit.min.js'));
-        rex_view::addJsFile($this->getAssetsUrl('uikit/js/uikit-icons.min.js'));
-    }
+        rex_view::addCssFile($this->getAssetsUrl('uikit/dist/css/uikit.min.css'));
+        rex_view::addJsFile($this->getAssetsUrl('uikit/dist/js/uikit.min.js'));
+        rex_view::addJsFile($this->getAssetsUrl('uikit/dist/js/uikit-icons.min.js'));
 }
-    rex_extension::register('PACKAGES_INCLUDED', function($ep) {
+rex_extension::register('PACKAGES_INCLUDED', function($ep) {
         include_once rex_path::addon('uikit_collection/inc').'funcs.php';
-    });
+});
  
