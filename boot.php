05357e1fc48c241a5a55f4e953f59634f10f6ee8
Thomas Skerbis <thomas.skerbis@mac.com>
2023-11-30 10:22:22 +0100

    corrected dump in Backend

--- boot.php
+++ boot.php
@@ -6,6 +6,7 @@ if (rex_plugin::get('yform', 'manager')->isAvailable()) {
 if (rex::isBackend() && rex::getUser()) {
     rex_perm::register('uikit_modul[]');
         rex_view::addCssFile($this->getAssetsUrl('uikit/dist/css/uikit.min.css'));
+        rex_view::addCssFile($this->getAssetsUrl('uikit_backend_fix.css'));
         rex_view::addJsFile($this->getAssetsUrl('uikit/dist/js/uikit.min.js'));
         rex_view::addJsFile($this->getAssetsUrl('uikit/dist/js/uikit-icons.min.js'));
 }
