6eff6413233d296e5ada61a227ff4e0d44f6a408
Thomas Skerbis <thomas.skerbis@mac.com>
2023-11-30 15:18:03 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -16,10 +16,10 @@ if (rex_plugin::get('yform', 'manager')->isAvailable()) {
 
 if (rex::isBackend() && rex::getUser()) {
     rex_perm::register('uikit_modul[]');
-        rex_view::addCssFile(rex_url::frontend($path.'css/uikit'.$prefix.'.min.css'));
+        rex_view::addCssFile(rex_url::frontend($path.'/css/uikit'.$prefix.'.min.css'));
         rex_view::addCssFile($package->getAssetsUrl('/uikit_backend_fix.css'));
-        rex_view::addJsFile(rex_url::frontend($path.'js/uikit.min.js'));
-        rex_view::addJsFile(rex_url::frontend($path.'js/uikit-icons.min.js'));
+        rex_view::addJsFile(rex_url::frontend($path.'/js/uikit.min.js'));
+        rex_view::addJsFile(rex_url::frontend($path.'/js/uikit-icons.min.js'));
 }
 rex_extension::register('PACKAGES_INCLUDED', function($ep) {
        # include_once rex_path::addon('uikit_collection/inc').'funcs.php';
