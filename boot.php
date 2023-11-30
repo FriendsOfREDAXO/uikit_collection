47f551c2ecabc4f70f264e3ba7ccc70c06f5719b
Thomas Skerbis <thomas.skerbis@mac.com>
2023-11-30 15:05:48 +0100

    prefix for css

--- boot.php
+++ boot.php
@@ -1,14 +1,25 @@
 <?php
+
+$package = rex_addon::get('uikit_collection');
+$path = $package->getConfig('path');
+$prefix = $package->getConfig('prefix');
+
+if ($prefix!='')
+{
+  $prefix  = '.'.$prefix;
+}
+
 if (rex_plugin::get('yform', 'manager')->isAvailable()) {
     rex_yform::addTemplatePath($this->getPath('ytemplates'));
 }
 
+
 if (rex::isBackend() && rex::getUser()) {
     rex_perm::register('uikit_modul[]');
-        rex_view::addCssFile($this->getAssetsUrl('uikit/dist/css/uikit.min.css'));
-        rex_view::addCssFile($this->getAssetsUrl('uikit_backend_fix.css'));
-        rex_view::addJsFile($this->getAssetsUrl('uikit/dist/js/uikit.min.js'));
-        rex_view::addJsFile($this->getAssetsUrl('uikit/dist/js/uikit-icons.min.js'));
+        rex_view::addCssFile(rex_url::frontend($path.'css/uikit'.$prefix.'.min.css'));
+        rex_view::addCssFile($this->getAssetsUrl('/uikit_backend_fix.css'));
+        rex_view::addJsFile(rex_url::frontend($path.'/js/uikit.min.js'));
+        rex_view::addJsFile(rex_url::frontend($path.'/js/uikit-icons.min.js'));
 }
 rex_extension::register('PACKAGES_INCLUDED', function($ep) {
        # include_once rex_path::addon('uikit_collection/inc').'funcs.php';
