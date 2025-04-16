60aa2fc11f67fe2c13125812fc10ac92cd1a430f
Thomas Skerbis <thomas.skerbis@mac.com>
2025-04-16 20:02:55 +0200

    5.0

--- pages/index.php
+++ pages/index.php
@@ -3,10 +3,7 @@
  * UIkit Collection - Hauptdatei für das Backend
  */
 
-// Verhindere direkten Aufruf
-if (!defined('REDAXO')) {
-    die('Direct access denied!');
-}
+
 
 // Titel für die Seite
 echo rex_view::title('UIkit Collection');
